a18379ef08d5caab97af0c100933304e83a5b878
Powerveil <1968103220@qq.com>
2024-05-12 22:58:11 +0800

    第一次提交

---
+++ src/main/java/com/heima/item/config/CaffeineConfig.java
@@ -0,0 +1,11 @@
+package com.heima.item.config;
+
+/**
+ * @ClassName CaffeineConfig
+ * @Description Caffeine配置类
+ * @Author Powerveil
+ * @Date 2024/5/12 22:54
+ * @Version 1.0
+ */
+public class CaffeineConfig {
+}

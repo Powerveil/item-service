8d04cb64fe7b56cfdbc942bcfbd501a12afc4e65
Powerveil <1968103220@qq.com>
2024-05-13 00:10:25 +0800

    使用Caffeine优化代码

--- src/main/java/com/heima/item/config/CaffeineConfig.java
+++ src/main/java/com/heima/item/config/CaffeineConfig.java
@@ -1,5 +1,12 @@
 package com.heima.item.config;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.heima.item.pojo.Item;
+import com.heima.item.pojo.ItemStock;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @ClassName CaffeineConfig
  * @Description Caffeine配置类
@@ -7,5 +14,27 @@ package com.heima.item.config;
  * @Date 2024/5/12 22:54
  * @Version 1.0
  */
+@Configuration
 public class CaffeineConfig {
+
+
+    @Bean
+    public Cache<Long, Item> itemCache() {
+        return Caffeine.newBuilder()
+                .initialCapacity(100)
+                .maximumSize(10_000)
+                .build();
+    }
+
+
+    @Bean
+    public Cache<Long, ItemStock> stockCache() {
+        return Caffeine.newBuilder()
+                .initialCapacity(100)
+                .maximumSize(10_000)
+                .build();
+    }
+
+
+
 }
